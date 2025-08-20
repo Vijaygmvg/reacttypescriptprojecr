29416f3b8e3a79da1d1d87b964b1e63c54ac9582
vijay GM <vijaygmvijaygmvg2003@gmail.com>
2025-08-20 14:49:12 +0530

    scr flder files were added successfully

---
+++ typescript-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+   <div>
+    hello good morning welcome to my new typescript 
+    this is my new project of typescript
+   </div>
+  );
+}
+
+export default App;
